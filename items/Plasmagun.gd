13b1ea761afe38aaf7287e86d6ff51de1f0f50ec
Nils <nils@inlage.com>
2020-10-25 22:10:17 +0100

    pickup fixes

--- items/Plasmagun.gd
+++ items/Plasmagun.gd
@@ -4,7 +4,7 @@ func _ready():
 	$AnimationPlayer.play("Rotate")
 
 func _on_body_entered(body):
-	if body.is_in_group("players") and $RespawnTimer.is_stopped():
+	if body.is_in_group("players") and $RespawnTimer.is_stopped()  and body.has_weapon2 == false:
 		body.has_weapon2 = true
 		body.rset('has_weapon2', true)
 		body.rpc('switch_weapon', 2)
@@ -15,5 +15,7 @@ func _on_body_entered(body):
 
 func _on_RespawnTimer_timeout():
 	$RespawnTimer.stop()
-	$AnimationPlayer.play_backwards("Collect")
+	$AnimationPlayer.play("Spawn")
+	yield(get_tree().create_timer(0.1), "timeout")
+	$AnimationPlayer.play("Rotate")
 	show()
