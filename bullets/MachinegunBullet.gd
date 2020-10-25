d030c98657116158302ce73c42c7053b5c78db54
Artur Hallmann <a.hallmann@neusta.de>
2020-10-25 17:19:46 +0100

    multiplayer fixes

--- bullets/MachinegunBullet.gd
+++ bullets/MachinegunBullet.gd
@@ -12,11 +12,7 @@ func _physics_process(delta):
 func _on_SimpleBullet_body_entered(body):
 	if body.is_in_group("mobs"):
 		body.take_damage(damage, by_who)
-		print("Hit mob")
 	else:
 		if body.is_in_group("players"):
 			body.take_damage(20, by_who)
-			print("Hit player")
-		else:
-			print("Hit Wall or Something")
 	queue_free()
--- bullets/ShotgunBullet.gd
+++ bullets/ShotgunBullet.gd
@@ -11,11 +11,7 @@ func _physics_process(delta):
 func _on_OtherBullet_body_entered(body):
 	if body.is_in_group("mobs"):
 		body.take_damage(30, by_who)
-		print("Hit mob")
 	else:
 		if body.is_in_group("players"):
 			body.take_damage(40, by_who)
-			print("Hit player")
-		else:
-			print("Hit Wall or Something")
 	queue_free()
