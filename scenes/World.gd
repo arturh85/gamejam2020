cf4345949981fd8dab9bf8a4b94fa462f10ae843
Artur Hallmann <a.hallmann@neusta.de>
2020-11-02 00:47:24 +0100

    revert

--- scenes/World.gd
+++ scenes/World.gd
@@ -22,18 +22,10 @@ func load_level(target_scene, levelName, back, rseed):
 	if target_scene:
 		new_level = load(target_scene).instance()
 	elif levelName:
-		if is_network_master():
-			new_level = random_level.instance()
-			new_level.init(levelName, rseed)
-			rpc("receive_map", new_level)
+		new_level = random_level.instance()
+		new_level.init(levelName, rseed)
 	elif back:
 		new_level = start_level.instance()
 		
 	if new_level:
-		remove_child(old_level)
-		add_child(new_level)
-		new_level.set_name("Level") # enforce
-		for player in players.get_children():
-			player.setMap(new_level)
-		old_level.queue_free()
-		minimap.call_deferred("update_map_markers")
+		receive_map(new_level)
