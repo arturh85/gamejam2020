df33b302dcda31d898ddc9d55996aaf034cb2257
Artur Hallmann <a.hallmann@neusta.de>
2020-11-02 00:35:05 +0100

    r

--- scenes/World.gd
+++ scenes/World.gd
@@ -6,19 +6,8 @@ var start_level = load("res://maps/Start.tscn")
 onready var players = $Players
 onready var minimap = $CanvasLayer/MiniMap
 	
-func load_level(target_scene, levelName, back, rseed):
+puppet func receive_map(new_level):
 	var old_level = get_node("Level")
-	
-	var new_level
-	if target_scene:
-		new_level = load(target_scene).instance()
-	elif levelName:
-		new_level = random_level.instance()
-		new_level.init(levelName, rseed)
-	elif back:
-		new_level = start_level.instance()
-		
-			
 	remove_child(old_level)
 	add_child(new_level)
 	new_level.set_name("Level") # enforce
@@ -26,3 +15,25 @@ func load_level(target_scene, levelName, back, rseed):
 		player.setMap(new_level)
 	old_level.queue_free()
 	minimap.call_deferred("update_map_markers")
+	
+func load_level(target_scene, levelName, back, rseed):
+	var old_level = get_node("Level")
+	var new_level
+	if target_scene:
+		new_level = load(target_scene).instance()
+	elif levelName:
+		if is_network_master():
+			new_level = random_level.instance()
+			new_level.init(levelName, rseed)
+			rpc("receive_map", new_level)
+	elif back:
+		new_level = start_level.instance()
+		
+	if new_level:
+		remove_child(old_level)
+		add_child(new_level)
+		new_level.set_name("Level") # enforce
+		for player in players.get_children():
+			player.setMap(new_level)
+		old_level.queue_free()
+		minimap.call_deferred("update_map_markers")
