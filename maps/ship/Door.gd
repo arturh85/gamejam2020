ca43d3b949c05af66c80697e93c8b310fce98c0b
Artur Hallmann <a.hallmann@neusta.de>
2020-11-02 15:14:55 +0100

    lockable doors

--- maps/ship/Door.gd
+++ maps/ship/Door.gd
@@ -12,38 +12,46 @@ var state = State.closed
 onready var sprite = $Sprite
 onready var shape = $Body/Shape
 
+export var locked = false setget set_locked
+
 var players_in_area = 0
 
+func set_locked(new_locked):
+	locked = new_locked
+	if locked and state != State.closed and State != State.closing:
+		close()
+
 func _process(_delta):
-	if players_in_area > 0 and state == State.closed:
-		Logger.info("found player in area, opening: " + str(players_in_area))
-		state = State.opening
-		sprite.play("opening")
-		shape.disabled = true
+	if players_in_area > 0 and state == State.closed and not locked:
+		open()
 	elif players_in_area <= 0 and state == State.open:
-		Logger.info("no found player in area, closing: " + str(players_in_area))
-		state = State.closing
-		sprite.play("opening", true)
-		shape.disabled = true
+		close()
+		
+func open():
+	state = State.opening
+	sprite.play("opening")
+	shape.disabled = true
+		
+func close():
+	state = State.closing
+	sprite.play("opening", true)
+	shape.disabled = true
 	
 func _on_animation_finished():
 	if state == State.opening:
 		state = State.open
 		sprite.animation = "open"
 		shape.disabled = true
-		Logger.info("opening finished")
 	elif state == State.closing:
 		state = State.closed
 		sprite.animation = "closed"
 		shape.disabled = false
-		Logger.info("closing finished")
 	
-
 func _on_Detect_body_entered(body):
 	if body.is_in_group("players"):
 		players_in_area += 1
 
-
 func _on_Detect_body_exited(body):
 	if body.is_in_group("players"):
 		players_in_area -= 1
+
