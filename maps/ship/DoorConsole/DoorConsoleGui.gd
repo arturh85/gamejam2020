8537eda94d56401634622f68b06572b416353bb2
Artur Hallmann <a.hallmann@neusta.de>
2020-11-03 01:36:43 +0100

    doors in door control gui

--- maps/ship/DoorConsole/DoorConsoleGui.gd
+++ maps/ship/DoorConsole/DoorConsoleGui.gd
@@ -2,20 +2,54 @@ extends Control
 
 onready var grid = $Panel/Grid
 
-var level_tilemap
+
+export var scale = 0.3
 
 func open():
-	if not level_tilemap:
-		var tilemap = get_node("../../Level/TileMap")
-		level_tilemap = tilemap.duplicate()
-		var grid_size = grid.get_rect().size
-		level_tilemap.position = Vector2(grid_size.x / 2 - 50, grid_size.y / 2)
-		level_tilemap.scale = Vector2(0.3, 0.3)
-		grid.add_child(level_tilemap)
+	var tilemap = get_node("../../Level/TileMap")
+	var level_tilemap = tilemap.duplicate()
+	var grid_size = grid.get_rect().size
+	level_tilemap.position = Vector2(grid_size.x / 2 - 50, grid_size.y / 2)
+	level_tilemap.scale = Vector2(scale, scale)
+	grid.add_child(level_tilemap)
+		
+	var map_objects = get_tree().get_nodes_in_group("minimap_objects")
+	
+	for item in map_objects:
+		pass
+	
+	var door_objects = get_tree().get_nodes_in_group("doors")
+	for item in door_objects:
+		var door = item.duplicate()
+		door.position.x = level_tilemap.position.x + (door.position.x * scale)
+		door.position.y = level_tilemap.position.y + (door.position.y * scale)
+		door.scale = Vector2(scale, scale)
+		
+		var detect = door.get_node("Body")
+		detect.connect("mouse_entered", self, "door_mouse_entered", [door])
+		detect.connect("mouse_exited", self, "door_mouse_exited", [door])
+		detect.connect("input_event", self, "door_mouse_exited", [door])
+		
+		
+		grid.add_child(door)
+		
 	show()
+	
+func door_mouse_input(viewport, event, shape_idx, door):
+	Logger.info("INPUT")	
+	
+func door_mouse_entered(door):
+	Logger.info("ENTERED")
+	door.modulate = "#ff0000"
+	
+func door_mouse_exited(door):
+	Logger.info("EXITED")
+	door.modulate = null
+	
 
 func close():
 	hide()
-	if level_tilemap:
-		level_tilemap.queue_free()
-		level_tilemap = null
+	for item in grid.get_children():
+		grid.remove_child(item)
+		item.queue_free()
+	
