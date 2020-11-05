73ce814b9d9bdec92563d266d802a6c84f2978f1
Nils <nils@inlage.com>
2020-11-06 00:55:57 +0100

    item update

--- inventory/Item.gd
+++ inventory/Item.gd
@@ -9,6 +9,7 @@ var itemSlot
 var slotType
 var picked = false
 var rarity = 0
+var texture2rect
 
 var rng = RandomNumberGenerator.new()
 
@@ -21,6 +22,14 @@ func _init(_itemName, _itemTexture, _itemImage, _itemSlot, _itemValue, _slotType
 	self.slotType = _slotType
 	self.rarity = rng.randi_range(Global.ItemRarity.NORMAL, Global.ItemRarity.LEGENDARY)
 	texture = _itemTexture
+	self.set_size(texture.get_size())
+	
+	match slotType:
+		Global.SlotType.SLOT_FEET, Global.SlotType.SLOT_PANTS:
+			texture2rect = TextureRect.new()
+			texture2rect.texture = _itemTexture
+			add_child(texture2rect)
+	
 	mouse_filter = Control.MOUSE_FILTER_PASS
 	mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
 
@@ -29,7 +38,25 @@ func pickItem():
 	picked = true
 
 func putItem():
-	rect_position = Vector2(0, 0)
+	if texture.get_size().x > 34:
+		rect_scale.x =34 / texture.get_size().x
+		rect_scale.y = 34 / texture.get_size().x
+	
+	rect_position.x = (34 - texture.get_size().x * rect_scale.x) / 2
+	rect_position.y = (34 - texture.get_size().y * rect_scale.y) / 2
+	if texture2rect:
+		
+		#texture2rect.rect_position = rect_position
+	
+		match slotType:
+			Global.SlotType.SLOT_FEET:
+				rect_position.y = rect_position.y + 4
+				texture2rect.rect_position.y = - 8
+			Global.SlotType.SLOT_PANTS:
+				rect_position.y = rect_position.y + 3
+				texture2rect.rect_position.y = - 6
+			
+	
 	mouse_filter = Control.MOUSE_FILTER_PASS
 	picked = false
 	
